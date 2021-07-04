f5bd684109fbfd8f3c0d17c6da9d8e6d0a150e8b
pazavi <pazavi@h-iMac-sl-Avi.local>
2021-07-04 14:32:55 +0300

    Showcase Component

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Showcase.module.css"
+
+export default function Showcase(){
+    return (
+        <div className={styles.showcase}>
+            <h1>learn from the best</h1>
+            <h2>Find the hottest masterclass events </h2>
+
+        </div>
+    )
+}
