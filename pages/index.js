150b40de187331e3660f15f9726719f435814f34
pazavi <pazavi@h-iMac-sl-Avi.local>
2021-07-04 19:22:31 +0300

    start of event item

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1> Home</h1>
+      <h1> Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
