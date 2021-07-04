391a8a46463faed24721d923bdbf3de7776b1924
pazavi <pazavi@h-iMac-sl-Avi.local>
2021-07-04 12:53:20 +0300

    header initial style

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">
-          <a>MC Events</a>
+          <a>M</a>
         </Link>
       </div>
 
