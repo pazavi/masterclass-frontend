50f0f48724111327d6f08b56f4b8f97f7a5bc2ac
pazavi <pazavi@h-iMac-sl-Avi.local>
2021-07-11 17:36:07 +0300

    JWT LOGIN LOGOUT REGISTER

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import { useContext } from "react";
 import Search from "./Search";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/Header.module.css";
 import { FaBrain, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -25,27 +29,44 @@ export default function Header() {
               <a> Events</a>
             </Link>
           </li>
-          <li className={styles.navlink}>
-            <Link href="/events/add">
-              <a> Add Event</a>
-            </Link>
-          </li>
-          <li className={styles.navlink}>
-            <Link href="/about">
-              <a> About</a>
-            </Link>
-          </li>
-
-          <li className={styles.navlink}>
-            <Link href="/account/login">
-              <a
-                className="btn-secondary btn-icon"
-                style={{ backgroundColor: "#475d43" }}
-              >
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            //if logged in
+            <>
+              <li className={styles.navlink}>
+                <Link href="/events/add">
+                  <a> Add Event</a>
+                </Link>
+              </li>
+              <li className={styles.navlink}>
+                <Link href="/account/dashboard">
+                  <a> Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <a
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                  style={{ backgroundColor: "#475d43" }}
+                >
+                  <FaSignOutAlt /> Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            //if logged out
+            <div>
+              <li className={styles.navlink}>
+                <Link href="/account/login">
+                  <a
+                    className="btn-secondary btn-icon"
+                    style={{ backgroundColor: "#475d43" }}
+                  >
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
         </ul>
       </nav>
     </header>
